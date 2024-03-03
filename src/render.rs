92efe5d15a40d70746118689fd6ffa748ab9fce7
Joe Ciskey <jciskey@gmail.com>
2024-03-03 08:36:52 -0700

    Add documentation comments

--- src/render.rs
+++ src/render.rs
@@ -4,15 +4,19 @@ use image::io::Reader;
 
 use crate::assets_data;
 
-
+/// A helpful type alias for the type of images this library operates on
 pub type OutputImage = image::ImageBuffer<image::Rgba<u8>, Vec<u8>>;
 
-
+/// The default number of columns in a room (x-coordinate)
 pub const DEFAULT_ROOM_MAX_COLUMNS: u32 = 50;
+
+/// The default number of rows in a room (y-coordinate)
 pub const DEFAULT_ROOM_MAX_ROWS: u32 = 50;
+
+/// The default scaling factor for the final image, meaning the number of pixels allocated for each room cell
 pub const DEFAULT_SCALE_FACTOR: u32 = 50;
 
-
+/// An enum representing the 3 terrain types: Plains, Swamps, and Walls
 #[derive(Debug)]
 pub enum Terrain {
   Plain,
@@ -20,6 +24,7 @@ pub enum Terrain {
   Wall,
 }
 
+/// An enum representing the various different types of resources that can exist in a room
 #[derive(Debug)]
 pub enum Resource {
   Source,
@@ -32,6 +37,7 @@ pub enum Resource {
   Catalyst,
 }
 
+/// An enum representing the various types of player-buildable structures
 #[derive(Debug)]
 pub enum BuildableStructure {
   Constructedwall,
@@ -53,10 +59,12 @@ pub enum BuildableStructure {
   Tower,
 }
 
+/// Creates an image with default size parameters
 pub fn create_image() -> OutputImage {
   create_image_with_size_params(DEFAULT_ROOM_MAX_COLUMNS, DEFAULT_ROOM_MAX_ROWS, DEFAULT_SCALE_FACTOR)
 }
 
+/// Creates an image with user-supplied size parameters
 pub fn create_image_with_size_params(room_max_cols: u32, room_max_rows: u32, scale_factor: u32) -> OutputImage {
   let mut imgbuf = image::ImageBuffer::new((room_max_cols * scale_factor) + 1 as u32, (room_max_rows * scale_factor) + 1 as u32);
 
@@ -70,10 +78,12 @@ pub fn create_image_with_size_params(room_max_cols: u32, room_max_rows: u32, sca
   imgbuf
 }
 
+/// Draws a grid on a default-sized image
 pub fn draw_grid(imgbuf: &mut OutputImage)  {
   draw_grid_with_scale_factor(imgbuf, DEFAULT_SCALE_FACTOR)
 }
 
+/// Draws a grid on an image with user-supplied scaling
 pub fn draw_grid_with_scale_factor(imgbuf: &mut OutputImage, scale_factor: u32) {
   for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
     if (x % scale_factor == 0) | (y % scale_factor == 0)  {
@@ -85,26 +95,31 @@ pub fn draw_grid_with_scale_factor(imgbuf: &mut OutputImage, scale_factor: u32) 
   }
 }
 
+/// Draws a text number on a default-sized image at a specific cell location
 pub fn draw_text_number_xy(imgbuf: &mut OutputImage, col: u32, row: u32, text: &str) {
   draw_text_number_xy_with_scale_factor(imgbuf, col, row, text, DEFAULT_SCALE_FACTOR)
 }
 
+/// Draws a text number on a user-sized image at a specific cell location
 pub fn draw_text_number_xy_with_scale_factor(imgbuf: &mut OutputImage, col: u32, row: u32, text: &str, scale_factor: u32) {
   let x = (col * scale_factor + 2).try_into().unwrap();
   let y = (row * scale_factor + 2).try_into().unwrap();
   draw_text_number_raw(imgbuf, x, y, text);
 }
 
+/// Underlying function for drawing numbers on an image at a specific cell location
 fn draw_text_number_raw(imgbuf: &mut OutputImage, x: i32, y: i32, text: &str) {
   let scale = rusttype::Scale::uniform(15.0);
   let font = rusttype::Font::try_from_bytes(assets_data::FREE_MONO_FONT_DATA).expect("Could not load FreeMono font");
   imageproc::drawing::draw_text_mut(imgbuf, image::Rgba([255,255,255,255]), x, y, scale, &font, text);
 }
 
+/// Draws a Terrain::* tile at a specific cell location
 pub fn draw_terrain_tile_xy(imgbuf: &mut OutputImage, col: u32, row: u32, tile: &Terrain) {
   draw_terrain_tile_xy_with_scale_factor(imgbuf, col, row, tile, DEFAULT_SCALE_FACTOR)
 }
 
+/// Draws a Terrain::* tile at a specific cell location with a user-supplied scaling factor
 pub fn draw_terrain_tile_xy_with_scale_factor(imgbuf: &mut OutputImage, col: u32, row: u32, tile: &Terrain, scale_factor: u32) {
   let tile_img_reader_result = match tile {
     Terrain::Plain => Reader::new(Cursor::new(assets_data::TERRAIN_PLAIN_IMG_DATA)).with_guessed_format(),
@@ -115,10 +130,12 @@ pub fn draw_terrain_tile_xy_with_scale_factor(imgbuf: &mut OutputImage, col: u32
   draw_tile_img_xy(imgbuf, col, row, tile_img_reader_result, scale_factor);
 }
 
+/// Draws a Resource::* tile at a specific cell location
 pub fn draw_resource_tile_xy(imgbuf: &mut OutputImage, col: u32, row: u32, tile: &Resource) {
   draw_resource_tile_xy_with_scale_factor(imgbuf, col, row, tile, DEFAULT_SCALE_FACTOR)
 }
 
+/// Draws a Resource::* tile at a specific cell location with a user-supplied scaling factor
 pub fn draw_resource_tile_xy_with_scale_factor(imgbuf: &mut OutputImage, col: u32, row: u32, tile: &Resource, scale_factor: u32) {
   let tile_img_reader_result = match tile {
     Resource::Source    => Reader::new(Cursor::new(assets_data::RESOURCE_SOURCE_IMG_DATA)).with_guessed_format(),
@@ -134,10 +151,12 @@ pub fn draw_resource_tile_xy_with_scale_factor(imgbuf: &mut OutputImage, col: u3
   draw_tile_img_xy(imgbuf, col, row, tile_img_reader_result, scale_factor);
 }
 
+/// Draws a BuildableStructure::* tile at a specific cell location
 pub fn draw_buildablestructure_tile_xy(imgbuf: &mut OutputImage, col: u32, row: u32, tile: &BuildableStructure) {
   draw_buildablestructure_tile_xy_with_scale_factor(imgbuf, col, row, tile, DEFAULT_SCALE_FACTOR)
 }
 
+/// Draws a BuildableStructure::* tile at a specific cell location with a user-supplied scaling factor
 pub fn draw_buildablestructure_tile_xy_with_scale_factor(imgbuf: &mut OutputImage, col: u32, row: u32, tile: &BuildableStructure, scale_factor: u32) {
   let tile_img_reader_result = match tile {
     BuildableStructure::Constructedwall => Reader::new(Cursor::new(assets_data::STRUCTURE_CONSTRUCTEDWALL_IMG_DATA)).with_guessed_format(),
@@ -162,6 +181,7 @@ pub fn draw_buildablestructure_tile_xy_with_scale_factor(imgbuf: &mut OutputImag
   draw_tile_img_xy(imgbuf, col, row, tile_img_reader_result, scale_factor);
 }
 
+/// Underlying helper function to draw a tile image at a specific cell location
 fn draw_tile_img_xy(imgbuf: &mut OutputImage, col: u32, row: u32, tile_img_reader_result: Result<Reader<Cursor<&[u8]>>, std::io::Error>, scale_factor: u32) {
 
   if let Ok(tile_img_reader) = tile_img_reader_result {
